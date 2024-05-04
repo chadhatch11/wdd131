345dd211208504a2bb53d150cd9173cd44fdbb30
chadhatch11 <Chadhatch11@hotmail.com>
2024-05-04 01:53:40 -0700

    java fix

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,10 @@
-// footer.js
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Set copyright year
+
     var currentYear = new Date().getFullYear();
     document.getElementById("copyright").textContent = currentYear;
     
-    // Set last modified date
+ 
     var lastModified = new Date(document.lastModified);
     document.getElementById("lastModified").textContent = lastModified.toLocaleDateString();
 });
